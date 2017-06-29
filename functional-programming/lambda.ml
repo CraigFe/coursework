c5f88fd481386c1a104e19bc30f1c96026d2cb94
Craig Ferguson <mediocrecraig@gmail.com>
2017-06-29 15:08:22 +0100

    lambda.ml: Minor documentation improvements

--- functional-programming/lambda.ml
+++ functional-programming/lambda.ml
@@ -1,4 +1,5 @@
-(*Church booleans are the Church encoding of the Boolean values true and false. In short, Boolean
+(* ----- CHURCH BOOLEANS -----
+  Church booleans are the Church encoding of the Boolean values true and false. In short, Boolean
   logic may be considered as a choice, such as in flow control. The Church encoding of true and 
   false are functions of two parameters: true chooses the first parameter, false chooses the 
   second parameter.
@@ -9,7 +10,8 @@
 fun TRUE  x y = x;
 fun FALSE x y = y;
 
-(*The above definitions of true and false naturally extend to a set of logic operators, which
+(* ----- LOGICAL OPERATORS -----
+  The above definitions of true and false naturally extend to a set of logic operators, which
   act on one or two Church boolean values and return the appropriate Church boolean. *)
 
 fun IF   b1    x y = b1 x y;
@@ -19,27 +21,29 @@ fun AND  b1 b2 x y = b1 (b2 x y) y;
 fun NAND b1 b2 x y = b1 (b2 y x) x;
 fun NOT  b1    x y = b1 y x;
 
-(*Church numerals are an abstraction of natural numbers using lambda notation. It is possible to
-  simply implement the hyperoperations from n=0 to n=3 using curried functions in ML, as 
-  illustrated in the following code.
+(* ----- CHURCH NUMERALS -----
+  Church numerals are an abstraction of natural numbers using lambda notation. Intuitively, the 
+  Church encoding of an natural number n is a function that maps any function to its n-fold 
+  composition, i.e:
 
-  Intuitively, the Church encoding of an natural number n is a function that maps any function to
-  its n-fold composition, i.e:
   	0 f x = x
   	1 f x = f(x)
-  	2 f x = f(f(x)) etc.*)
+  	2 f x = f(f(x)) etc.  *)
 
-fun zero     f x = x;
-fun succ   n f x = f(n f x);       (*Apply successor function to base n+1 many times*)
+fun zero     f x = x;         (*Do not apply successor function*)
+fun succ   n f x = f(n f x);  (*Apply successor function to base n+1 many times*)
 
+(* ----- ARITHMETIC OPERATORS -----
+  Arithmetic operations on numbers may be encoded as functions which act on Church numerals. The 
+  hyperoperations from n=0 to n=3 can be simply implemented using curried functions in ML:  *)
 
-(*-----OPERATION DEFINITIONS-----*)
 fun succ   n f x =   f(n f x); (*Apply successor function to base n many times*)
 fun add  m n f x = m f(n f x); (*Take successor of n, m many times*)
 fun mult m n f x = m (n f) x;  (*Take nth successor of base, m many times*)
-fun pow  m n = n m;
+fun pow  m n     = n m;
 
-(*-----PREDECESSOR------*)
+(* ----- PREDECESSOR FUNCTION -----
+  The predecessor funtion is the inverse of the successor function. It is *)
 fun pre n f x = (fn (a,b) => a) (n (fn (a,b) => (b,f b)) (x,x));
 fun sub m n f x = n pre m f x;
 
