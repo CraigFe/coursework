15f0c8f35482f28c837eeb4e4232aafb7041e691
Craig Ferguson <mediocrecraig@gmail.com>
2017-06-29 14:51:34 +0100

    lambda.ml: added logical operators and documentation

--- functional-programming/lambda.ml
+++ functional-programming/lambda.ml
@@ -1,4 +1,3 @@
-
 (*Church booleans are the Church encoding of the Boolean values true and false. In short, Boolean
   logic may be considered as a choice, such as in flow control. The Church encoding of true and 
   false are functions of two parameters: true chooses the first parameter, false chooses the 
@@ -7,8 +6,18 @@
   This definition allows preficates to act directly as if-clauses:
   	<predicate> bool <then-clause> <else-clause> *)
 
-fun True  x y = x;
-fun False x y = y;
+fun TRUE  x y = x;
+fun FALSE x y = y;
+
+(*The above definitions of true and false naturally extend to a set of logic operators, which
+  act on one or two Church boolean values and return the appropriate Church boolean. *)
+
+fun IF   b1    x y = b1 x y;
+fun OR   b1 b2 x y = b1 x (b2 x y);
+fun XOR  b1 b2 x y = b1 (b2 y x) (b2 x y);
+fun AND  b1 b2 x y = b1 (b2 x y) y;
+fun NAND b1 b2 x y = b1 (b2 y x) x;
+fun NOT  b1    x y = b1 y x;
 
 (*Church numerals are an abstraction of natural numbers using lambda notation. It is possible to
   simply implement the hyperoperations from n=0 to n=3 using curried functions in ML, as 
@@ -20,12 +29,9 @@ fun False x y = y;
   	1 f x = f(x)
   	2 f x = f(f(x)) etc.*)
 
-(*-----BASIS-----*)
 fun zero     f x = x;
 fun succ   n f x = f(n f x);       (*Apply successor function to base n+1 many times*)
 
-(*-----PREDECESSOR------*)
-fun pre n f x = (fn (a,b) => a) (n (fn (a,b) => (b,f b)) (x,x));
 
 (*-----OPERATION DEFINITIONS-----*)
 fun succ   n f x =   f(n f x); (*Apply successor function to base n many times*)
@@ -33,6 +39,8 @@ fun add  m n f x = m f(n f x); (*Take successor of n, m many times*)
 fun mult m n f x = m (n f) x;  (*Take nth successor of base, m many times*)
 fun pow  m n = n m;
 
+(*-----PREDECESSOR------*)
+fun pre n f x = (fn (a,b) => a) (n (fn (a,b) => (b,f b)) (x,x));
 fun sub m n f x = n pre m f x;
 
 chur(*testing*)
