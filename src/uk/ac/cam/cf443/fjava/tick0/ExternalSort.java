000dd95c683abf9c2f646b0e7a12d9122c780c1f
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-07 00:54:51 +0100

    Test now runs on file copies

--- src/uk/ac/cam/cf443/fjava/tick0/ExternalSort.java
+++ src/uk/ac/cam/cf443/fjava/tick0/ExternalSort.java
@@ -16,17 +16,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
+import static java.nio.file.StandardCopyOption.*;
 
 
 
 public class ExternalSort {
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		for(int i = 1; i <= 10; i++) test(i);
+		for(int i = 1; i <= 6; i++) test(i);
 	}
 	
 	private static void test(int n) throws FileNotFoundException, IOException {
-		System.out.println("--------------------------------------------");
+		System.out.println("----------------------------------------------------------------------");
 		System.out.println("Test "+n);
 		System.out.println();
 		
@@ -35,72 +36,84 @@ public class ExternalSort {
 		//Create a tmp fileA to work on
 		File src = new File(filepath+"test"+n+"a.dat");
 		File dst = new File(filepath+"test"+n+"a-copy.dat");
-		Files.copy(src.toPath(), dst.toPath());
-		    
+		Files.copy(src.toPath(), dst.toPath(), REPLACE_EXISTING);
+		
+		//Likewise for fileB
+	    src = new File(filepath+"test"+n+"b.dat");
+		dst = new File(filepath+"test"+n+"b-copy.dat");
+		Files.copy(src.toPath(), dst.toPath(), REPLACE_EXISTING);
+		
+		
 		long startTime = System.nanoTime();
 		
+		sort(filepath+"test"+n+"a-copy.dat",filepath+"test"+n+"b-copy.dat");
 		
-		sort(filepath+"test"+n+"a-copy.dat",filepath+"test"+n+"b.dat");
-		
-		System.out.println("The checksum is "+checkSum(filepath+"test"+n+"a-copy.dat"));
+		System.out.println();
 		System.out.println("Process completed in "+(System.nanoTime()-startTime)/1000/1000.00+"ms.");
+		System.out.println();
+		System.out.println("Checksums: ");
+		System.out.println("   init "+checkSum(filepath+"test"+n+"a.dat"));
+		System.out.println("  final "+checkSum(filepath+"test"+n+"a-copy.dat"));
+		
+		if (checkSum(filepath+"test"+n+"a-copy.dat").equals(checksums[n-1])) {
+			System.out.println("VALID");
+		} else {
+			System.out.println("INVALID");
+			Runtime.getRuntime().halt(0);
+		}
+		
+		
 	}
 
 	public static void sort(String filenameA, String filenameB) 
 			throws FileNotFoundException, IOException {
 		
 		RandomAccessFile a1 = new RandomAccessFile(filenameA,"rw");
-		RandomAccessFile a2 = new RandomAccessFile(filenameA,"rw");
 		RandomAccessFile b1 = new RandomAccessFile(filenameB,"rw");
-		RandomAccessFile b2 = new RandomAccessFile(filenameB,"rw");
 		
 		DataInputStream input = new DataInputStream(
 				new BufferedInputStream(
 						new FileInputStream(
 								a1.getFD())));
+		
+		DataOutputStream output = new DataOutputStream(
+				new BufferedOutputStream(
+						new FileOutputStream(
+								a1.getFD())));
 
+		a1.seek(0);
 		long mem = Runtime.getRuntime().freeMemory();
 		long req = a1.length();
 		
 		System.out.println("Available memory:" + mem);
-		System.out.println("FileA: "+a1.length());
-		System.out.println("FileB: "+b1.length());
-		
-		
-		if (req == 0)  {
-			System.out.println("File is empty");
-			
+		System.out.println("FileA: "+a1.length()+" bytes");
+		System.out.println("FileB: "+b1.length()+" bytes");
 			
 		//Sort the file in memory
-		} else if (mem > 1.1 * a1.length()) {
+		if (mem > 1.2 * a1.length()) {
 			System.out.println("File fits in memory");
 			
 			int num = (int) (req/4);
 			
 			ArrayList<Integer> buffer = new ArrayList<>();
 			for (int i = 0; i < num; i++) buffer.add(input.readInt());
-			
-			Collections.sort(buffer);
-			
-			for (Integer i: buffer) a1.writeInt(i);
-			
-			
 	
+			Collections.sort(buffer);
+			System.out.println(buffer.toString());
+			for (Integer i: buffer) output.writeInt(-1);
+			
+			output.flush(); //Force buffered bytes to be written to the stream 
+			
 		} else {
 			System.out.println("File does not fit in memory");
 			
-			long blockSize = req;
+			/*long blockSize = req;
 			if (mem < req) blockSize = (long) (mem-1000)/2;
 			
-			int[] buffer = new int[(int) ((blockSize-1000)/4)];
-
-			
+			int[] buffer = new int[(int) ((blockSize-1000)/4)];*/
 			
 		}
-		
 		return;
-		
-		
 	}
 	
 	
@@ -133,5 +146,27 @@ public class ExternalSort {
 		if (r.length() == 8) return r.substring(6);
 		return r;
 	}
-
+	
+	//md5 checksums of the sorted files
+	private static String[] checksums = {
+		"d41d8cd98f0b24e980998ecf8427e",
+		"a54f041a9e15b5f25c463f1db7449",
+		"c2cb56f4c5bf656faca0986e7eba38",
+		"c1fa1f22fa36d331be4027e683baad6",
+		"8d79cbc9a4ecdde112fc91ba625b13c2",
+		"1e52ef3b2acef1f831f728dc2d16174d",
+		"6b15b255d36ae9c85ccd3475ec11c3",
+		"1484c15a27e48931297fb6682ff625",
+		"ad4f60f065174cf4f8b15cbb1b17a1bd",
+		"32446e5dd58ed5a5d7df2522f0240",
+		"435fe88036417d686ad8772c86622ab",
+		"c4dacdbc3c2e8ddbb94aac3115e25aa2",
+		"3d5293e89244d513abdf94be643c630",
+		"468c1c2b4c1b74ddd44ce2ce775fb35c",
+		"79d830e4c0efa93801b5d89437f9f3e",
+		"c7477d400c36fca5414e0674863ba91",
+		"cc80f01b7d2d26042f3286bdeff0d9"
+		
+	};
+	
 }
