c646b0aa21cd50115cbcadc00e3403d9e8d32bd5
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-06 23:42:43 +0100

    ExternalSort: Testing method implemented

--- src/uk/ac/cam/cf443/fjava/tick0/ExternalSort.java
+++ src/uk/ac/cam/cf443/fjava/tick0/ExternalSort.java
@@ -3,43 +3,108 @@ package uk.ac.cam.cf443.fjava.tick0;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 
 public class ExternalSort {
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		RandomAccessFile f = new RandomAccessFile("src/uk/ac/cam/cf443/fjava/tick0/example","rw");
-		DataOutputStream d = new DataOutputStream(
-		    new BufferedOutputStream(new FileOutputStream(f.getFD())));
-		d.writeInt(1); //write calls now only store primitive ints in memory
-		d.writeInt(2);
-		d.writeInt(3);
-		d.flush(); //force the contents to be written to the disk. Important!
-		f.seek(4);
-		System.out.println("Read four bytes as an int value "+f.readInt());
-		System.out.println("The file is "+f.length()+" bytes long");
+		for(int i = 1; i <= 10; i++) test(i);
+	}
+	
+	private static void test(int n) throws FileNotFoundException, IOException {
+		System.out.println("--------------------------------------------");
+		System.out.println("Test "+n);
+		System.out.println();
+		
+		String filepath = "src/uk/ac/cam/cf443/fjava/tick0/test-suite/";
+		
+		//Create a tmp fileA to work on
+		File src = new File(filepath+"test"+n+"a.dat");
+		File dst = new File(filepath+"test"+n+"a-copy.dat");
+		Files.copy(src.toPath(), dst.toPath());
+		    
+		long startTime = System.nanoTime();
+		
+		
+		sort(filepath+"test"+n+"a-copy.dat",filepath+"test"+n+"b.dat");
+		
+		System.out.println("The checksum is "+checkSum(filepath+"test"+n+"a-copy.dat"));
+		System.out.println("Process completed in "+(System.nanoTime()-startTime)/1000/1000.00+"ms.");
 	}
 
 	public static void sort(String filenameA, String filenameB) 
 			throws FileNotFoundException, IOException {
 		
+		RandomAccessFile a1 = new RandomAccessFile(filenameA,"rw");
+		RandomAccessFile a2 = new RandomAccessFile(filenameA,"rw");
+		RandomAccessFile b1 = new RandomAccessFile(filenameB,"rw");
+		RandomAccessFile b2 = new RandomAccessFile(filenameB,"rw");
+		
+		DataInputStream input = new DataInputStream(
+				new BufferedInputStream(
+						new FileInputStream(
+								a1.getFD())));
+
+		long mem = Runtime.getRuntime().freeMemory();
+		long req = a1.length();
+		
+		System.out.println("Available memory:" + mem);
+		System.out.println("FileA: "+a1.length());
+		System.out.println("FileB: "+b1.length());
+		
+		
+		if (req == 0)  {
+			System.out.println("File is empty");
+			
+			
+		//Sort the file in memory
+		} else if (mem > 1.1 * a1.length()) {
+			System.out.println("File fits in memory");
+			
+			int num = (int) (req/4);
+			
+			ArrayList<Integer> buffer = new ArrayList<>();
+			for (int i = 0; i < num; i++) buffer.add(input.readInt());
+			
+			Collections.sort(buffer);
+			
+			for (Integer i: buffer) a1.writeInt(i);
+			
+			
+	
+		} else {
+			System.out.println("File does not fit in memory");
+			
+			long blockSize = req;
+			if (mem < req) blockSize = (long) (mem-1000)/2;
+			
+			int[] buffer = new int[(int) ((blockSize-1000)/4)];
+
+			
+			
+		}
+		
+		return;
+		
+		
 	}
 	
-	private static String byteToHex(byte b) {
-		String r = Integer.toHexString(b);
-		if (r.length() == 8) return r.substring(6);
-		return r;
-	}
 	
-	public static String checkSum(String filename) {
+	private static String checkSum(String filename) {
 
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
@@ -62,4 +127,11 @@ public class ExternalSort {
 		
 		return "<error computing checksum>";
 	}
+	
+	private static String byteToHex(byte b) {
+		String r = Integer.toHexString(b);
+		if (r.length() == 8) return r.substring(6);
+		return r;
+	}
+
 }
