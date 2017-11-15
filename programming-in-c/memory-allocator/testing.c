bd74f952d17ddaf69bc0c599befc0de87f7f4bb9
Craig Ferguson <cf443@cam.ac.uk>
2017-11-15 02:27:42 +0000

    Added additional test cases

--- programming-in-c/memory-allocator/testing.c
+++ programming-in-c/memory-allocator/testing.c
@@ -1,12 +1,14 @@
 #include <assert.h>
 #include <stdbool.h>
 
+#include "includes/kernel.h"
+
 // ----------------------------------------------
 //   TESTING
 // ----------------------------------------------
 extern bool memory_exhausted;
 
-#define TEST_COUNT 10
+#define TEST_COUNT 8
 
 int main() {
     printf("Initialising memory");
@@ -40,36 +42,72 @@ bool test_00() {
   return (p == NULL);
 }
 
-/* Simple allocation of a single integer, and checking that the 
- * value is read correctly. */
-bool test_01() {
-    int *x = mem_alloc(sizeof(int));
-    *x = 3;
-    return (*x == 3);
-}
-
-
 /* Ensure that the allocator successfully returns null if memory is
  * exhausted and there is no free space on the heap. */
-bool test_02() {
+bool test_01() {
   memory_exhausted = TRUE;
   void *p = mem_alloc(0xF0000);
   return (p == NULL);
 }
 
+/* Simple allocation of a single integer, and checking that the 
+ * value is read correctly. */
+bool test_02() {
+    int *x = mem_alloc(sizeof(int));
+    *x = 3;
+    return (*x == 3);
+}
+
+/* Allocation of a chunk which is larger than the blocks provided
+ * by the kernel */
 bool test_03() {
-
+  void *p = mem_alloc(MEM_BLOCK_SIZE + 1);
+  return (p != NULL); // If no segfault; assume success
 }
 
+/* Create one node for each bin size */
 bool test_04() {
+  for (int i = 0; i < BIN_COUNT; i++) {
+    void *p = mem_alloc(1 << i);
+    if (p == NULL) return false;
+  }
 
+  return true;
 }
 
-// Ensure that the memory is 
-bool test_10() {
-  memory_exhausted = TRUE;
+/* Allocate many blocks of the same size */
+bool test_05() {
+  for (int i = 0; i < 0xF0000; i++) {
+    void *p = mem_alloc(1);
+    if (p == NULL) return false;
+  }
+  
+  return true;
+}
 
+/* Attempt a simple call to free */
+bool test_06() {
+  void *p = mem_alloc(1);
+  free(p);
+  return true; // If no segfault; assume success
+}
 
+/* Free a chunk which is larger than a block provided by the kernel */
+bool test_07() {
+  void *p = mem_alloc(MEM_BLOCK_SIZE + 1);
+  free(p);
+  return true;
+}
+
+/* Repeatedly allocate and free blocks to test against memory leaks */
+bool test_07() {
+  for (int i = 0; i < 0xF000; i++) {
+    void *p = mem_alloc(0xF000);
+    if (p == NULL && i > 0) return false; // Memory leak
+    mem_free(p);
+  }
+
+  return true;
 }
 
 // Simpply calls one of the tests, according to the test number
@@ -83,9 +121,5 @@ bool test(int testNumber) {
     case  5: return test_05();
     case  6: return test_06();
     case  7: return test_07();
-    case  8: return test_08();
-    case  9: return test_09();
-    case 10: return test_10();
-    case 11: return test_11();
   }
 }
